8d9f752453f960e53e8c2b15bdeb28d1729a7123
PAVAN SG <93654850+pavansrinivas02@users.noreply.github.com>
2024-08-05 20:19:56 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,204 @@
+<<<<<<< HEAD
+
+let c=document.getElementById("dp");
+c.addEventListener('click',dropit=>{
+    
+    
+    let r=document.querySelector(".drop")
+    let w=document.querySelector(".list")
+
+    r.classList.toggle("z")
+    r.classList.add("new")
+    w.classList.toggle("z")
+    w.classList.add("list")
+    let d=document.getElementById("bor")
+    d.style.marginBottom="1px"
+    
+
+})
+
+
+
+
+
+
+
+let ca=document.getElementById("dop");
+ca.addEventListener('click',dropt=>{
+    
+    
+    let r=document.querySelector(".dropi")
+    let w=document.querySelector(".listi")
+
+    r.classList.toggle("zau")
+    r.classList.add("newi")
+    w.classList.toggle("zau")
+    w.classList.add("listi")
+    let d=document.getElementById("bori")
+    d.style.marginBottom="1px"
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let u=document.getElementById("p");
+u.addEventListener('click',dropi=>{   
+
+    let y=document.querySelector(".drp")
+    let l=document.querySelector(".lis") 
+    y.classList.toggle("z1")
+    y.classList.add("nw")
+    l.classList.toggle("z1")
+    l.classList.add("lis")
+    let p=document.getElementById("bo")
+    p.style.marginBottom="1px"
+
+})
+let o=document.getElementById("j");
+o.addEventListener('click',droi=>{   
+
+    let f=document.querySelector(".dp")
+    let r=document.querySelector(".ls")
+   
+    f.classList.toggle("zoo")
+    f.classList.add("n")
+    r.classList.toggle("zoo")
+    r.classList.add("ls")
+    let m=document.getElementById("b")
+    m.style.marginBottom="1px"
+
+})
+let bari=document.getElementById("bar");
+
+
+bari.addEventListener('click',dora=>{
+   
+    let ra=document.querySelector(".sidebar")
+
+    
+    
+  
+
+    ra.classList.toggle("sideba")
+      
+   
+=======
+
+let c=document.getElementById("dp");
+c.addEventListener('click',dropit=>{
+    
+    
+    let r=document.querySelector(".drop")
+    let w=document.querySelector(".list")
+
+    r.classList.toggle("z")
+    r.classList.add("new")
+    w.classList.toggle("z")
+    w.classList.add("list")
+    let d=document.getElementById("bor")
+    d.style.marginBottom="1px"
+    
+
+})
+
+
+
+
+
+
+
+let ca=document.getElementById("dop");
+ca.addEventListener('click',dropt=>{
+    
+    
+    let r=document.querySelector(".dropi")
+    let w=document.querySelector(".listi")
+
+    r.classList.toggle("zau")
+    r.classList.add("newi")
+    w.classList.toggle("zau")
+    w.classList.add("listi")
+    let d=document.getElementById("bori")
+    d.style.marginBottom="1px"
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let u=document.getElementById("p");
+u.addEventListener('click',dropi=>{   
+
+    let y=document.querySelector(".drp")
+    let l=document.querySelector(".lis") 
+    y.classList.toggle("z1")
+    y.classList.add("nw")
+    l.classList.toggle("z1")
+    l.classList.add("lis")
+    let p=document.getElementById("bo")
+    p.style.marginBottom="1px"
+
+})
+let o=document.getElementById("j");
+o.addEventListener('click',droi=>{   
+
+    let f=document.querySelector(".dp")
+    let r=document.querySelector(".ls")
+   
+    f.classList.toggle("zoo")
+    f.classList.add("n")
+    r.classList.toggle("zoo")
+    r.classList.add("ls")
+    let m=document.getElementById("b")
+    m.style.marginBottom="1px"
+
+})
+let bari=document.getElementById("bar");
+
+
+bari.addEventListener('click',dora=>{
+   
+    let ra=document.querySelector(".sidebar")
+
+    
+    
+  
+
+    ra.classList.toggle("sideba")
+      
+   
+>>>>>>> 4eaa64b (third commit)
+})
